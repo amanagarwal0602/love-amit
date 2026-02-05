c8a171fe7c6cf0b03171453e63af5fc87bf528c4
amanagarwal0602 <amanagarwal0602@gmail.com>
2026-02-05 21:48:37 +0530

    Fix paths, JS safety, and Vercel deployment issues

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function moveRandomEl(elm) {
   elm.style.position = "absolute";
   elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
@@ -7,6 +6,8 @@ function moveRandomEl(elm) {
 
 const moveRandom = document.querySelector("#move-random");
 
-moveRandom.addEventListener("mouseenter", function (e) {
-  moveRandomEl(e.target);
-});
+if (moveRandom) {
+  moveRandom.addEventListener("mouseenter", function (e) {
+    moveRandomEl(e.target);
+  });
+}
